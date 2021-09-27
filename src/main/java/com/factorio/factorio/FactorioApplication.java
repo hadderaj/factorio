303a1f6133a4ca2a1df6f1ebbd4e337962d3fb22
hadderaj <oskarlun@gmail.com>
2021-09-27 14:32:32 +0200

    första

---
+++ src/main/java/com/factorio/factorio/FactorioApplication.java
@@ -0,0 +1,13 @@
+package com.factorio.factorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/factorio/factorio/FactorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factorio.factorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
